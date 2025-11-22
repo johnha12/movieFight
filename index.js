9ca4d0543e7986f58dbb7a1b6d0f45810baf4158
Jonathan <80421998+johnha12@users.noreply.github.com>
2025-11-21 19:13:12 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ createAutoComplete({
 let leftMovie;
 let rightMovie;
 const onMovieSelect = async (movie, summaryElement, side) => {
-  const response = await axios.get("http://www.omdbapi.com/", {
+  const response = await axios.get("https://www.omdbapi.com/", {
     params: {
       apikey: "2d0ad63e",
       i: movie.imdbID
